5b7cb87b3ea34b8dc5c071c01e673f67b9fd34ad
michael.zhang <zhangqiuyuan@gmail.com>
2012-12-26 17:06:12 +1300

    Add comment delete in view and controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,13 @@ class CommentsController < ApplicationController
     @comment = @item.comments.create(params[:comment])
     redirect_to item_path(@item)
   end
+
+  #delete /items/4/comment/4
+  def destroy
+    @item = Item.find(params[:item_id])
+    @comment = @item.comments.find(params[:id])
+    @comment.destroy
+    redirect_to item_path(@item)
+  end
+
 end
