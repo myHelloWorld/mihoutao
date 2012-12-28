af1d46abdffd8dc1bf2b3276a9c5870e62dae651
michael.zhang <zhangqiuyuan@gmail.com>
2012-12-29 10:36:04 +1300

    Function: Enable attachment deletion.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,4 +4,12 @@ class AttachmentsController < ApplicationController
     @attachment = @item.attachments.create(params[:attachment])
     redirect_to item_path(@item)
   end
+
+  #/items/7/attachments/5
+  def destroy
+    @item = Item.find(params[:item_id])
+    @attachment = @item.attachments.find(params[:id])
+    @attachment.destroy
+    redirect_to item_path(@item)
+  end
 end
