a422d286d594744b8a22f57712591d62c53dddf4
Cecil <minggang.chen@gmail.com>
2013-01-08 18:11:21 +1300

    Basic shop function(create, edit, view and validation). However, the contacts defined in the contact model are not yet implemented in the shop function in this committed version.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,14 @@
 class Shop < ActiveRecord::Base
   attr_accessible :name, :shopCode, :personFirstName, :personLastName, :description, :phone, :fax, :email, :addressLineOne, :addressLineTwo, :addressLineThree, :city
   has_many :contacts
+  
+  validates :name, :presence=>true
+  validates :shopCode, :presence=>true
+  validates :personFirstName, :presence=>true
+  validates :personLastName, :presence=>true
+  validates :email, :email=>true
+  validates :addressLineOne, :presence=>true
+  validates :city, :presence=>true #need more validation
+  
+  
 end
